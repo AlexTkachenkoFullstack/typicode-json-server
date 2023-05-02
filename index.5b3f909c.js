8de9795ce251d3c25ad0c73685606e9e4c5e8920
AlexTkachenkoFullstack <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-05-02 20:59:10 +0000

    Deploying to gh-pages from @ AlexTkachenkoFullstack/typicode-json-server@110d1815e6f72974d7fd660057ef62c0e41e8a47 🚀

--- index.5b3f909c.js
+++
@@ -1,2 +0,0 @@
-async function o(o){try{const t=await fetch(`http://localhost:4004/books/${o}`),c=await t.json();console.log(c)}catch(o){console.log(o.message)}}!async function(){try{const o=await fetch("http://localhost:4004/books"),t=await o.json();console.log(t)}catch(o){console.log(o.message)}}(),o(2),o(8);
-//# sourceMappingURL=index.5b3f909c.js.map
